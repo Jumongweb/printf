448f40282427c04a83ef992dbbebf1fd23ff834c
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-09-17 09:27:11 -0700

    vi

--- functions1.c
+++ functions1.c
@@ -35,7 +35,7 @@ int print_unsigned(va_list types, char buffer[],
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
- /** 
+/**
  * print_octal - Prints an unsigned number in octal notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
